27b324d3508e8db8452ba9b631e0a39c6abc90fd
sergiodmuah <s.miguel@edu.uah.es>
2026-02-10 13:29:51 +0100

    Borrar todo

--- interface/__init__.py
+++
@@ -1,9 +0,0 @@
-# interface/__init__.py
-"""
-Paquete de interfaz gráfica para FCM Designer
-"""
-
-from .map_canvas import MapCanvas, MouseMode
-from .map_window import MapWindow
-
-__all__ = ['MapCanvas', 'MouseMode', 'MapWindow']
